8a7dd9f67a500fbb93f95ac8ad8c4646b26828ec
ssaunishtheglitch <saunish@theglitch.in>
2017-06-27 15:22:26 +0530

    test changes 0.3

--- routes/index.js
+++ routes/index.js
@@ -12,7 +12,7 @@ router.get('/', function(req, res, next) {
 
 router.post('/', function (req, res, next) {
     console.log('hello');
-    console.log(req.body.entry);
+    console.log(req.body.entry[0]);
 });
 
 module.exports = router;
