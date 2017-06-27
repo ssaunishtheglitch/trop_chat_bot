2a45d40f9c881d5c0dd943a4132dc4b76ee9b452
ssaunishtheglitch <saunish@theglitch.in>
2017-06-27 14:57:30 +0530

    test changes

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,15 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    if (req.query['hub.mode'] && req.query['hub.verify_token'] === 'abcdefg') {
+        res.status(200).send(req.query['hub.challenge']);
+    } else {
+        res.status(403).end();
+    }
+});
+
+router.post('/', function (req, res, next) {
+    console.log(req.body);
 });
 
 module.exports = router;
